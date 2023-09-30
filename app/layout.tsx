970ac48dddd4c241f6d4ef1a85425799c54c10f1
ZhiXuan Zhong <dooi921@gmail.com>
2023-10-01 00:19:16 +0800

    feat: finish auth by utilizing clerk

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,5 @@
+import { ClerkProvider } from '@clerk/nextjs';
+
 import './globals.css';
 import type { Metadata } from 'next';
 import { Open_Sans } from 'next/font/google';
@@ -11,8 +13,10 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
-      <body className={font.className}>{children}</body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body className={font.className}>{children}</body>
+      </html>
+    </ClerkProvider>
   );
 }
